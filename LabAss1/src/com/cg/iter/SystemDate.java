38584599278d1a1a1929a0986044b6f3498e961f
karanj15 <karan.ksj15@gmail.com>
2020-04-13 20:18:44 +0530

    first commit

---
+++ LabAss1/src/com/cg/iter/SystemDate.java
@@ -0,0 +1,41 @@
+package com.cg.iter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SystemDate")
+public class SystemDate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public SystemDate()
+	{
+		super();
+	}
+	
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+	
+	public void destroy()
+	{
+		
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Date date = new Date();
+		PrintWriter out= response.getWriter();
+		out.println(date);
+	}
+}
